b5246e2ba61f3129bcf2f5027022a021a37adc62
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2020-07-07 11:06:32 -0400

    Test again

--- src/config.js
+++ src/config.js
@@ -13,5 +13,5 @@ export default {
     APP_CLIENT_ID: '334lv23nl443oa3q4tijgrg9s8',
     IDENTITY_POOL_ID: 'us-east-1:213ed2de-cd71-4115-ba57-04674f18bb89',
   },
-  MAX_ATTACHMENT_SIZE: 2000000,
+  MAX_ATTACHMENT_SIZE: 3000000,
 };
